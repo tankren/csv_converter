4433585abc22f9bfbd333f8640522297d7910b8a
tankren <tankrenlive@gmail.com>
2022-08-16 13:32:13 +0800

    1st working version

--- Main.py
+++ Main.py
@@ -3,18 +3,23 @@
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import pandas
+import os
+from tkinter import filedialog, Tk
 
 
 class Watcher:
-    DIRECTORY_TO_WATCH = input('Enter path to folder')
-
+    Tk().withdraw()
+    DIRECTORY_TO_WATCH= filedialog.askdirectory()
+    
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
         event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=False)
         self.observer.start()
+        print ("------------Start monitoring------------")  
         try:
             while True:
                 time.sleep(5)
@@ -26,7 +31,6 @@ class Watcher:
 
 
 class Handler(FileSystemEventHandler):
-
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
@@ -34,13 +38,25 @@ class Handler(FileSystemEventHandler):
 
         elif event.event_type == 'created':
             # Take any action here when a file is first created.
-            print ("Received created event - %s." % event.src_path)
-
-        elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
-            print ("Received modified event - %s." % event.src_path)
-
-
+            try:
+                print ("------------New file detected------------")                
+                oldfile = event.src_path
+                print(oldfile)
+                if os.path.splitext(oldfile)[1][1:].strip().lower()== 'csv':
+                    filename = os.path.basename(oldfile)
+                    path = os.path.dirname(oldfile)
+                    newpath = f'{path}\\converted'
+                    if not os.path.exists(newpath):
+                        os.makedirs(newpath)
+                    newfile = f'{path}\\converted\\{filename}'
+                    readcsv = pandas.read_csv(oldfile, sep=';', decimal=",")                    
+                    print("------------Start converting------------")
+                    print(readcsv)
+                    readcsv.to_csv(newfile, sep=',', decimal=".", encoding='utf-8')                
+                    print("------------Conversion completed------------")
+            except Exception as e:
+                print(e)    
+                   
 if __name__ == '__main__':
     w = Watcher()
     w.run()
